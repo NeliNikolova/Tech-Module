e31815426535097b7a32d2ce00aff0041324af48
NeliNikolova <radkova_neli_github@abv.bg>
2017-10-03 15:25:17 +0300

    Add files with solutions

---
+++ ConvertToChar/Recovered-Sep-30-2017-0245PM.Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            for ( int i = numberOne;  i <=numberTwo;  i++)
+            {
+                char m = Convert.ToChar(i);
+                Console.Write("{0} ", m);
+            }
+            Console.WriteLine();
+        }
+    }
+}
